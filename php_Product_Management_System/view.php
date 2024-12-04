233ab39e0d53b5d4b6aefdec781ad7ff0d6093a7
kris-veto <82740207+kris-veto@users.noreply.github.com>
2024-12-03 23:21:10 -0500

    Update view.php

--- php_Product_Management_System/view.php
+++ php_Product_Management_System/view.php
@@ -18,74 +18,51 @@ require './inc/database.php';
         }
   </script>
   <link rel="stylesheet" href="css/style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<style>
-  .img-fluid{
-    width: 10vw;
-    height:auto;
-  }
-  table{
-    margin-top: 2%;
-    scale: 0.8;
-  }
-  h2{
-    margin-top: 2%;
-    text-align:center;
-  }
- 
-</style>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-<h2>Product Catalog</h2>
-<div class="bootstrap-container">
-        <div class="row">
-            <div class="col">
-                <table class="table table-bordered table-hover">
-                    <thead>
-                        <tr class= table-primary>
-                            <th>Product Id</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Description</th>
-                            <th>image</th>
-                              
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    //display data
-                    $sql="select* from product_info";
-                    $result=$conn->query($sql);
-                    
-                    if($result->num_rows>0)
-                    {
-                        while($row=$result->fetch_assoc())
-                        {
-                        echo "<tr>";
-                        echo "<td>".$row["id"]."</td>";
-                        echo "<td>".$row["Product_name"]."</td>";
-                        echo "<td>$".$row["Price"]."</td>";
-                        echo "<td>".$row["Quantity"]."</td>";
-                        echo "<td>".$row["Description"]."</td>";
-                        
-                        echo "<td><img src='".$row['image']."' title='".$row['Product_name']."' class='img-fluid'></td>";
-                        
-                        
-                    echo"</tr>";
-                    }}
-                    else{
-                        echo"<tr> <td colsapn='3'>No subscription found</td></tr>";
+<h1>Product Catalog</h1>
 
-                    }
-                    ?>
-                </tbody>
-            
-            
-                </table>
-            </div>
-         </div>
-    </div>
+      <table class="table">
+          <thead>
+              <tr class= table-primary>
+                  <th>Product Id</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Description</th>
+                  <th>image</th>
+                    
+              </tr>
+          </thead>
+          <tbody>
+          <?php
+          //display data
+          $sql="select* from product_info";
+          $result=$conn->query($sql);
+          
+          if($result->num_rows>0)
+          {
+              while($row=$result->fetch_assoc())
+              {
+              echo "<tr>";
+              echo "<td>".$row["id"]."</td>";
+              echo "<td>".$row["Product_name"]."</td>";
+              echo "<td>$".$row["Price"]."</td>";
+              echo "<td>".$row["Quantity"]."</td>";
+              echo "<td>".$row["Description"]."</td>";
+              
+              echo "<td><img src='".$row['image']."' title='".$row['Product_name']."' class='img-fluid'></td>";
+              
+          echo"</tr>";
+          }}
+          else{
+              echo"<tr> <td colsapn='3'>No subscription found</td></tr>";
+          }
+          ?>
+      </tbody>
+      </table>
+      
 </body>
 </html>
 <?php
